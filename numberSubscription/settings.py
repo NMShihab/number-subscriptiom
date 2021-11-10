54cc0c09292e8965d669860dda9d887a6f5edf4f
NMShihab <nmshihabislam@gmail.com>
2021-11-10 15:00:30 +0600

    add models

--- numberSubscription/settings.py
+++ numberSubscription/settings.py
@@ -57,10 +57,11 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ]
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
 }
 
 ROOT_URLCONF = 'numberSubscription.urls'
--- subscriber/models.py
+++ subscriber/models.py
@@ -3,13 +3,24 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class SubscriptionPlan(models.model):
+    """ Subscription plan """
+    subscription_plan_name = models.CharField(max_length=100)
+    subscription_plan_type = models.CharField(max_length=100)
+    subscription_plan_amount = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.subscription_plan_name
+    
+
 class Customer(models.Model):
     """ Customer Model """
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     primary_number = models.CharField(max_length=14,unique=True,blank=False,null=False)
-    phone_number1=models.CharField(max_length=14,blank=True,null=True)
+    phone_number1 = models.CharField(max_length=14,blank=True,null=True)
     phone_number2 = models.CharField(max_length=14,blank=True, null=True)
-    subscription_plan = models.CharField(max_length=100)
+    subscription_plan = models.CharField(SubscriptionPlan,max_length=100)
     stripe_id = models.CharField(max_length=256)
     subscription_id = models.CharField(max_length=256,blank=True,null=True)
     start_date = models.CharField(max_length =256)
@@ -18,3 +29,17 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.primary_number
+
+
+class SubscriptionData(models.model):
+    """ All Subscription data """
+     
+    subscriber = models.CharField(max_length=14)
+    subscription = models.CharField(max_length=100)
+    subscription_start = models.CharField(max_length=100,blank=True,null=True)
+    subscription_end = models.CharField(max_length=100,blank=True,null=True)
+    
+    
+
+    
+    
